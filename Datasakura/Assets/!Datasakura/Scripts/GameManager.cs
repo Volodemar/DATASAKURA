efacc812efc4effd15e22d62f0d57d7265f519b4
Volodemar <vshangeev@gmail.com>
2025-02-26 13:46:49 +0300

    Создан проект. Настройка под VSCode. Настроен gitignore.

---
+++ Datasakura/Assets/!Datasakura/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Менеджер игры
+/// </summary>	
+public class GameManager : MonoBehaviour
+{
+}
